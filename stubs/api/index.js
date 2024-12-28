6829e3aca070e820c3c6c763946f8ebf7249e7ff
admin <rebbit123456@gmail.com>
2024-12-28 10:46:32 +0300

    add delay

--- stubs/api/index.js
+++ stubs/api/index.js
@@ -3,6 +3,15 @@ const interestsRouter = require('./interests');
 const usersRouter = require('./users');
 module.exports = router;
 
+
+const delay =
+  (ms = 1000) =>
+  (req, res, next) => {
+    setTimeout(next, ms);
+  };
+
+  
+router.use(delay());
 router.use('/interests', interestsRouter);
 router.use('/users', usersRouter);
 router.use('/users/:id', usersRouter);
