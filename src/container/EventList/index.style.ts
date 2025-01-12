2b45a7b0eba5911935d94df5e2eeee73114bef33
admin <rebbit123456@gmail.com>
2025-01-12 13:13:45 +0300

    [fix][style] edit event list

--- src/container/EventList/index.style.ts
+++ src/container/EventList/index.style.ts
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 
 export const EventListStyled = styled.div`
+  width: 100%;
   margin-top: 8px;
 `;
--- src/container/components/Loading/index.style.ts
+++ src/container/components/Loading/index.style.ts
@@ -1,13 +1,6 @@
 import styled from '@emotion/styled';
 
 export const LoadingStyled = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-`;
-
-export const LoadingBoxStyled = styled.div`
   width: 50px;
   height: 50px;
   border: 5px solid var(--tg-theme-text-color);
@@ -16,11 +9,17 @@ export const LoadingBoxStyled = styled.div`
   animation: spin 1s linear infinite;
 
   @keyframes spin {
-  0% {
-    transform: rotate(0deg);
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
   }
-  100% {
-    transform: rotate(360deg);
-  }
-}
+`;
+
+export const LoadingBoxStyled = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
